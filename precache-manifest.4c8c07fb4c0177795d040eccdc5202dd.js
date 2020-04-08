d4e7864eacab1edc948b9d69a0702724a116f495
Apagyi Dávid <apagyi.david@gmail.com>
2020-04-09 00:22:12 +0200

    Updates

---
+++ precache-manifest.4c8c07fb4c0177795d040eccdc5202dd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71881b660010ad006356d98b5f99d8e",
+    "url": "/vetessy-husvet/index.html"
+  },
+  {
+    "revision": "e8915131214b892c195e",
+    "url": "/vetessy-husvet/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "90de4d5b1fb413319e3b",
+    "url": "/vetessy-husvet/static/css/main.8475dfa1.chunk.css"
+  },
+  {
+    "revision": "e8915131214b892c195e",
+    "url": "/vetessy-husvet/static/js/2.9267fd14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vetessy-husvet/static/js/2.9267fd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90de4d5b1fb413319e3b",
+    "url": "/vetessy-husvet/static/js/main.e3ba02c1.chunk.js"
+  },
+  {
+    "revision": "dbee0c97504d732e9bc2",
+    "url": "/vetessy-husvet/static/js/runtime-main.00ce1adc.js"
+  },
+  {
+    "revision": "730bdef8d06f84b950eb78b32a67dea5",
+    "url": "/vetessy-husvet/static/media/dvg logo.730bdef8.svg"
+  },
+  {
+    "revision": "36606a997a6a1e7ad818327467193ba6",
+    "url": "/vetessy-husvet/static/media/footer_nyakkendő.36606a99.svg"
+  },
+  {
+    "revision": "e9e50633d7730a9c512a9045ed1d8e90",
+    "url": "/vetessy-husvet/static/media/liliom_hatter.e9e50633.png"
+  }
+]);
